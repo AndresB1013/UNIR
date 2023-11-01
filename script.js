87a5dd6580b360150b04d3fabbc6216211232509
AndresB1013 <giovanny_ballen@hotmail.com>
2023-11-01 13:27:45 -0500

    Filtro de datos y Variables

--- script.js
+++ script.js
@@ -1,161 +1,218 @@
-// Declare the chart dimensions and margins.
-const width = 800;
-const height = 400;
-const margin = { top: 20, right: 20, bottom: 40, left: 40 };
-const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
-// const changeThemeBtn = document.getElementById("changeThemeBtn");
+(() => {
+    // Definición de Variables para acceder a las etiquetas HTML
+    const BtnSelector = d3.select("#aplicarFiltro");
+    const BtnCambiarTema = d3.select("#changeThemeBtn");
+    const Año = d3.select("#filtro");
+    let Tema = d3.select("#Tema");
+    const graficos = d3.select("#container");
+    const Titulo = d3.select("#Titulo");
 
-// Selecciona el contenedor del gráfico de dispersión
-var svg = d3.select("#line").append("svg")
-    .attr("widht", width)
-    .attr("height", height)
-    .attr("viewBox", [0, 0, width + 50, height + 50])
-    .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
-    .style("-webkit-tap-highlight-color", "transparent")
-    .style("overflow", "visible");
+    const width = 800;
+    const height = 400;
+    const margin = { top: 20, right: 20, bottom: 40, left: 40 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    graficos.style("display", "none");
+
+    function loadData(b, filtro,color) {
+        loadTheme();
+
+        var svg = d3.select("#line")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [0, 0, width + 50, height + 50])
+            .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+            .style("-webkit-tap-highlight-color", "transparent")
+            .style("overflow", "visible");
+
+            svg.append("path");
+        svg.append("g").attr("class", "x-axis");
+
+        svg.append("g").attr("class", "y-axis");
 
 
-d3.json("indicadores_del_mercado_inmobiliario.json").then(data => {
+        d3.json("indicadores_del_mercado_inmobiliario.json").then(data => {
+            var vivienda = data.Respuesta.Datos.Metricas[b].Datos;
 
-    var vivienda_nueva = data.Respuesta.Datos.Metricas[0].Datos;
+            var elementoUl = d3.select("#line").append("ul");
+            var filtro_año = vivienda.filter(function (d) {
+                return new Date(d.Agno) >= filtro;
+            })
 
-    var elementoUl = d3.select("#line").append("ul");
-    console.log(vivienda_nueva);
+            const xScale = d3.scaleBand()
+                .domain(filtro_año.map(d => convertirAMes(d.Parametro) + " - " + d.Agno))
+                .range([margin.left, width - margin.right])
+                .padding(0.1);
 
-    var vivienda_nueva_filtrada = vivienda_nueva.filter(function (d) {
-        return new Date(d.Agno) >= new Date(2022);
+            const yScale = d3.scaleLinear([d3.min(filtro_año, d => d.Valor), d3.max(filtro_año, d => d.Valor)], [height - margin.bottom, margin.top]);
 
-    })
+            const line = d3.line()
+                .x(d => xScale(convertirAMes(d.Parametro) + " - " + d.Agno) + 17)
+                .y(d => yScale(d.Valor));
 
-    const xScale = d3.scaleBand()
-        .domain(vivienda_nueva_filtrada.map(d => convertirAMes(d.Parametro) + " - " + d.Agno))
-        .range([margin.left, width - margin.right])
-        .padding(0.1);
+            // Agrega ejes
+            const xAxis = d3.axisBottom(xScale);
+            const yAxis = d3.axisLeft(yScale);
 
-    const yScale = d3.scaleLinear([d3.min(vivienda_nueva_filtrada, d => d.Valor), d3.max(vivienda_nueva_filtrada, d => d.Valor)], [height - margin.bottom, margin.top]);
+            svg.select("g.x-axis")
+                .attr("transform", `translate(0, ${height - margin.bottom})`)
+                .call(xAxis);
 
-    const line = d3.line()
-        .x(d => xScale(convertirAMes(d.Parametro) + " - " + d.Agno))
-        .y(d => yScale(d.Valor));
+            svg.select("g.y-axis")
+                .attr("class", "y-axis")
+                .attr("transform", `translate(${margin.left},0)`)
+                .call(yAxis.ticks(height / 40))
+                .call(g => g.select(".domain").remove())
+                .call(g => g.selectAll(".tick line").clone()
+                    .attr("x2", innerWidth)
+                    .attr("stroke-opacity", 0.1))
+                .call(g => g.append("text")
+                    .attr("x", - margin.left)
+                    .attr("y", 10)
+                    .attr("fill", "currentColor")
+                    .attr("text-anchor", "start")
+                    .text("↑ Variación Mes (%)"));
 
-    // Agrega ejes
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
 
-    svg.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", `translate(0, ${height - margin.bottom})`)
-        .call(xAxis);
+            svg.select("path")
+                .attr("fill", "none")
+                .attr("stroke", color)
+                .attr("stroke-width", 1.5)
+                .attr("d", line(filtro_año))
+                .style("stroke-dasharray", function () {
+                    return this.getTotalLength() + " " + this.getTotalLength();
+                })
+                .style("stroke-dashoffset", function () {
+                    return this.getTotalLength();
+                })
+                .transition()
+                .duration(7000) // Duración de la animación en milisegundos
+                .ease(d3.easeLinear)
+                .style("stroke-dashoffset", 0);
 
-    svg.append("g")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(yAxis.ticks(height / 40))
-        .call(g => g.select(".domain").remove())
-        .call(g => g.selectAll(".tick line").clone()
-            .attr("x2", innerWidth)
-            .attr("stroke-opacity", 0.1))
-        .call(g => g.append("text")
-            .attr("x", - margin.left)
-            .attr("y", 10)
-            .attr("fill", "currentColor")
-            .attr("text-anchor", "start")
-            .text("↑ Variacion Mes (%)"));
 
-    svg.append("path")
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("d", line(vivienda_nueva_filtrada));
 
-    svg.selectAll("circle")
-        .data(vivienda_nueva_filtrada)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xScale(convertirAMes(d.Parametro) + " - " + d.Agno))
-        .attr("cy", d => yScale(d.Valor))
-        .attr("r", 3);
+            svg.selectAll("circle")
+                .data(filtro_año)
+                .enter()
+                .append("circle")
+                .attr("cx", d => xScale(convertirAMes(d.Parametro) + " - " + d.Agno) + 17)
+                .attr("cy", d => yScale(d.Valor))
+                .attr("r", 3);
 
-    function convertirAMes(valor) {
-        if (valor === "2022" || valor === "2023") {
-            return "Enero";
-        }
-        return valor;
+            function convertirAMes(valor) {
+                if (valor === "2017" || valor === "2018" || valor === "2019" || valor === "2020" || valor === "2021" || valor === "2022" || valor === "2023") {
+                    return "Enero";
+                }
+                return valor;
+            }
+
+            //ETIQUETAS DEL EJE X
+            svg.selectAll(".x-axis text") // Selecciona las etiquetas del eje X
+                .style("text-anchor", "end") // Alinea el texto al final del elemento
+                .attr("transform", "rotate(-45)") // Rota el texto en sentido antihorario
+                .attr("dx", "-.8em") // Ajusta la posición horizontal
+                .attr("dy", ".15em"); // Ajusta la posición vertical;
+
+            const tooltip = d3.selectAll("svg")
+                .append("div")
+                .attr("class", "tooltip")
+                .style("opacity", 0);
+
+            const tooltipWidth = 150; // Ancho del tooltip
+            const tooltipHeight = 50; // Altura del tooltip
+
+            tooltip.append("rect")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("width", tooltipWidth)
+                .attr("height", tooltipHeight)
+                .attr("fill", "black")
+                .attr("stroke", "gray");
+
+            const textValue = tooltip.select("rect")
+                .append("text")
+                .attr("x", 10)
+                .attr("y", 20);
+
+            const textParameter = tooltip.select("rect").append("text")
+                .attr("x", 10)
+                .attr("y", 40);
+
+            // Agregar interacción para mostrar el tooltip
+            svg.selectAll("circle")
+                .on("mouseover", function (event, d) {
+                    const xPos = xScale(convertirAMes(d.Parametro) + " - " + d.Agno) + 17;
+                    const yPos = yScale(d.Valor);
+
+                    tooltip.transition().duration(200).style("opacity", 0.9);
+
+                    tooltip.style("display", "block")
+                        .attr("transform", `translate(${xPos},${yPos})`);
+
+                    textValue.text(`Variacion: ${d.Valor.toFixed(2)} %`);
+                    textParameter.text(`Mes: ${convertirAMes(d.Parametro) + " - " + d.Agno}`);
+                })
+                .on("mouseout", function () {
+                    tooltip.style("display", "none");
+                    tooltip.style("opacity", 0);
+                });
+
+            // filtro_año.forEach(function (d) {
+            //     elementoUl.append("li").text(d.Agno + " - " + convertirAMes(d.Parametro) + " => " + d.Valor);
+            // })
+
+
+            console.log(filtro)
+            console.log(filtro_año);
+
+        })
     }
 
-    svg.selectAll(".x-axis text") // Selecciona las etiquetas del eje X
-        .style("text-anchor", "end") // Alinea el texto al final del elemento
-        .attr("transform", "rotate(-45)") // Rota el texto en sentido antihorario
-        .attr("dx", "-.8em") // Ajusta la posición horizontal
-        .attr("dy", ".15em"); // Ajusta la posición vertical;
+    BtnSelector.on("click", () => {
+        d3.select("svg").remove();
+        var filtro = Año.property("value")
 
-    const tooltip = svg.append("g")
-        .attr("class", "tooltip")
-        .style("display", "none");
+        if (Tema.property("value") == "Todas") {
+            graficos.style("display", "block");
+            loadData(2, filtro,"orange");
+            Titulo.text("Gráfico de Variacion porcentual mensual de Compra de Viviendas nuevas y segunda Mano - España")
+        }
+        else if (Tema.property("value") == "Nuevas") {
+            graficos.style("display", "block");
+            loadData(0, filtro,"blue");
+            Titulo.text("Gráfico de Variacion porcentual mensual de Compra de Viviendas nuevas - España")
 
-    const tooltipWidth = 150; // Ancho del tooltip
-    const tooltipHeight = 50; // Altura del tooltip
-
-    tooltip.append("rect")
-        .attr("width", tooltipWidth)
-        .attr("height",tooltipHeight)
-        .attr("fill", "white")
-        .attr("stroke", "gray");
-
-    const textValue = tooltip.append("text")
-        .attr("x", 10)
-        .attr("y", 20);
-
-    const textParameter = tooltip.append("text")
-        .attr("x", 10)
-        .attr("y", 40);
-
-// Agregar interacción para mostrar el tooltip
-svg.selectAll("circle")
-    .on("mouseover", function (event, d) {
-        const xPos = xScale(convertirAMes(d.Parametro) + " - " + d.Agno);
-        const yPos = yScale(d.Valor);
-
-        tooltip.style("display", "block")
-            .attr("transform", `translate(${xPos},${yPos})`);
-
-        textValue.text(`Variacion: ${d.Valor.toFixed(2)} %`);
-        textParameter.text(`Mes: ${convertirAMes(d.Parametro)}`);
-    })
-    .on("mouseout", function () {
-        tooltip.style("display", "none");
-    });
-
-
-    vivienda_nueva_filtrada.forEach(function (d) {
-        elementoUl.append("li").text(d.Agno + " - " + d.Parametro + " => " + d.Valor);
+        }
+        else {
+            graficos.style("display", "block");
+            loadData(1, filtro,"green");
+            Titulo.text("Gráfico de Variacion porcentual mensual de Compra de Viviendas segunda Mano - España")
+        }
     })
 
     function loadTheme() {
         const theme = localStorage.getItem("theme") || "light";
         document.body.dataset.bsTheme = theme;
         if (theme == "dark") {
-            changeThemeBtn.textContent = "Light Mode";
+            BtnCambiarTema.text("Modo Claro");
         } else {
-            changeThemeBtn.textContent = "Dark Mode";
+            BtnCambiarTema.text("Modo Noche");
         }
     }
 
-    changeThemeBtn.addEventListener("click", function () {
-        let body = document.body;
-        if (body.dataset.bsTheme == "dark") {
-            body.dataset.bsTheme = "light";
-            changeThemeBtn.textContent = "Dark Mode";
-            localStorage.setItem("theme", "light");
-        } else {
-            body.dataset.bsTheme = "dark";
-            changeThemeBtn.textContent = "Light Mode";
-            localStorage.setItem("theme", "dark");
-        }
+    BtnCambiarTema.on("click", function () {
+        const body = d3.select("body");
+        const currentTheme = body.attr("data-bs-theme");
+        const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+        body.attr("data-bs-theme", newTheme);
+        d3.select("#changeThemeBtn").text(newTheme === "dark" ? "Modo Claro" : "Modo Noche");
+        localStorage.setItem("theme", newTheme);
     });
 
-
-    console.log(vivienda_nueva_filtrada);
-
-})
-
+    // Llama a loadTheme() cuando la página se carga
+    loadData();
+})();
